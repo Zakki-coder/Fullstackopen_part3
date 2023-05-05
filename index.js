0ec286d69fe379e767f7b9f8b5b7a1b1d0788e06
jniemine <nieminen.jakke@gmail.com>
2023-05-05 22:21:09 +0300

    phonebook step3

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-	const date = 'Phonebook has info for ' + persons.length + ' people' + "\n" + new Date().toString()
-	res.write(date)
-	res.end()
+	const date = new Date().toString()
+	res.send(`<div>Phonebook has info for ${persons.length} people
+	<br></br>
+	${date}</div>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(person => person.id === id)
+	if (person)
+		res.json(person)
+	else
+		res.status(404).end()
 })
