e8c75a41edf64007a85dcbe45781c40b9e04dcbc
jniemine <nieminen.jakke@gmail.com>
2023-05-12 19:17:18 +0300

    phonebook step9

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const app = express() //We create object representing express app
 const morgan = require('morgan')
-const util = require('util')
-let persons = require('./db.json') //TODO How to update the json file?
+const cors = require('cors')
+
+let persons = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
+
+
+// app.use(express.static('build'))
+app.use(cors())
 
 morgan.token('body', (req, res) => {
 	if (req.method === 'POST')
@@ -38,14 +64,24 @@ app.get('/api/persons/:id', (req, res) => {
 		res.status(404).end()
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res) => {
+	const newBody = req.body
 	const id = Number(req.params.id)
 	persons = persons.filter(person => person.id !== id)
+	newBody.id = id
+	persons = persons.concat(newBody)
+	return res.json(newBody)
 })
 
-app.post('/api/persons', (req, res) => {
-	const id = Math.floor(Math.random() * 1000000)
-	const body = req.body
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	console.log('Before', persons)
+	persons = persons.filter(person => person.id !== id)
+	console.log('After', persons)
+	return res.status(200).end()
+})
+
+const validateBody = (body, res) => {
 	if (!body.name)
 		return res.status(404).json({
 			error: 'name missing'
@@ -58,6 +94,15 @@ app.post('/api/persons', (req, res) => {
 		return res.status(404).json({
 			error: 'name must be unique'
 		})
+	return null
+}
+
+app.post('/api/persons', (req, res) => {
+	const id = Math.floor(Math.random() * 1000000)
+	const body = req.body
+	const validate = validateBody(body, res)
+	if (validate)
+		return validate
 	const person = {...body, id: id}
 	persons = persons.concat(person)
 	return res.json(person)
