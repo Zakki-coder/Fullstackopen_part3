cf79b5ac4c404e8db2087ace544d40416f63280d
jniemine <nieminen.jakke@gmail.com>
2023-05-16 01:00:41 +0300

    phonebook part 14

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const express = require('express')
 const app = express() //We create object representing express app
 const morgan = require('morgan')
-const cors = require('cors')
+// const cors = require('cors')
+const Person = require('./models/person')
+const { ReturnDocument } = require('mongodb')
 
-
-
-let persons = [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": 1
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": 3
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
+// let persons = [
+//     { 
+//       "name": "Arto Hellas", 
+//       "number": "040-123456",
+//       "id": 1
+//     },
+//     { 
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523",
+//       "id": 2
+//     },
+//     { 
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345",
+//       "id": 3
+//     },
+//     { 
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122",
+//       "id": 4
+//     }
+//   ]
 
 
 app.use(express.static('build'))
@@ -47,40 +47,63 @@ app.listen(PORT, () => {
 })
 
 app.get('/api/persons', (req, res) => {
-	res.json(persons)
+	Person.find({}).then(person => {
+		res.json(person)
+	})
 })
 
 app.get('/info', (req, res) => {
 	const date = new Date().toString()
-	res.send(`<div>Phonebook has info for ${persons.length} people
-	<br></br>
-	${date}</div>`)
+	const personN = Person.estimatedDocumentCount()
+	personN.then(response => {
+		res.send(`<div>Phonebook has info for ${response} people
+		<br></br>
+		${date}</div>`)})
 })
 
 app.get('/api/persons/:id', (req, res) => {
 	const id = Number(req.params.id)
-	const person = persons.find(person => person.id === id)
-	if (person)
-		res.json(person)
-	else
-		res.status(404).end()
+	Person.findById(id)
+		.then(person => {
+			res.json(person)
+		})
+		.catch(error => {
+			res.status(404).end()
+		})
+	// if (person)
+	// 	res.json(person)
+	// else
+		// res.status(404).end()
 })
 
 app.put('/api/persons/:id', (req, res) => {
 	const newBody = req.body
-	const id = Number(req.params.id)
-	persons = persons.filter(person => person.id !== id)
-	newBody.id = id
-	persons = persons.concat(newBody)
-	return res.json(newBody)
+	const id = req.params.id
+	// persons = persons.filter(person => person.id !== id)
+	Person.findByIdAndUpdate(id, newBody, {returnDocument: 'after'})
+	.then(person => {
+		console.log("lol", JSON.stringify(person))
+		return res.json(person)
+	})
+	.catch(error => {
+		console.log("Hiiteen")
+		console.log(error)
+		res.status(404).end()
+	})
+	// newBody.id = id
+	// persons = persons.concat(newBody)
+	// return res.json(newBody)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-	const id = Number(req.params.id)
-	console.log('Before', persons)
-	persons = persons.filter(person => person.id !== id)
-	console.log('After', persons)
-	return res.status(200).end()
+	const id = req.params.id
+	Person.findByIdAndRemove(id)
+		.then(response => {
+			res.status(200).end()
+		})
+		.catch(error => {
+			res.status(404).end()
+		})
 })
 
 const validateBody = (body, res) => {
@@ -92,10 +115,17 @@ const validateBody = (body, res) => {
 		return res.status(404).json({
 			error: 'number missing'
 		})
-	if (persons.some(person => person.name === body.name))
-		return res.status(404).json({
-			error: 'name must be unique'
-		})
+	// Person.find( {name: body.name} )
+	// 	.then(person => {
+	// 		console.log(person)
+	// 		res.status(404).json({
+	// 			error: 'name must be unique'
+	// 		}).end()
+	// 	})
+	// if (persons.some(person => person.name === body.name))
+	// 	return res.status(404).json({
+	// 		error: 'name must be unique'
+		// })
 	return null
 }
 
@@ -106,6 +136,36 @@ app.post('/api/persons', (req, res) => {
 	if (validate)
 		return validate
 	const person = {...body, id: id}
-	persons = persons.concat(person)
-	return res.json(person)
-})
+	console.log("NAME", person.name)
+	Person.findOne( {name: person.name} )
+	.then(lol => {
+		console.log(lol)
+		if (lol !== null) {
+		res.status(404).json({
+			error: 'name must be unique'
+			}).end()
+		}
+		else {
+		Person.create(person)
+			.then(response => {
+				console.log("Added new to db")
+				res.json(response).end()
+			})
+			.catch(error => {
+				console.log("Failed miserably")
+				console.log(error)
+			})
+		}
+		})
+	})
+	// persons = persons.concat(person)
+	// Person.create(person)
+	// 	.then(response => {
+	// 		console.log("Added new to db")
+	// 		res.json(response)
+	// 	})
+	// 	.catch(error => {
+	// 		console.log("Failed miserably")
+	// 		console.log(error)
+	// 	})
+	// return res.json(person)
---
+++ models/person.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+// if (argvLen === 4 || argvLen > 5)
+// 	process.exit(1)
+
+const password = "kcatslluf" //Fix this
+const url =
+	`mongodb+srv://fullstack:${password}@phonebook.jb7vwo2.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+	.then(result => {
+		console.log("connected to mongoDB")
+	})
+	.catch((error) => {
+		console.log("error connecting to MongoDB", error.message)
+	})
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+})
+
+personSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+// const Person = mongoose.model('Person', personSchema)
+
+// if (argvLen === 3) {
+// 	Person.find({}, 'name number').then(result => {
+// 		console.log('phonebook:')
+// 		result.forEach(person => {
+// 			console.log(`${person.name} ${person.number}`)
+// 		})
+// 		mongoose.connection.close()
+// 	})
+// }
+
+// if (argvLen === 5) {
+// 	const person = new Person({
+//   	name: process.argv[3],
+//   	number: Number(process.argv[4]),
+// 	})
+
+// 	person.save().then(result => {
+// 	console.log(`added ${result.name} number ${result.number} to phonebook`)
+//   	mongoose.connection.close()
+// 	})
+// }
+
+module.exports = mongoose.model('Person', personSchema)
