73ea8c3ffce120af661a32aa850bcb570c659c69
jniemine <nieminen.jakke@gmail.com>
2023-05-17 21:12:43 +0300

    Part 3 ready

--- index.js
+++ index.js
@@ -1,132 +1,103 @@
 require('dotenv').config()
 const express = require('express')
-const app = express() //We create object representing express app
+const app = express()
 const morgan = require('morgan')
-// const cors = require('cors')
 const Person = require('./models/person')
 
-//TODO What happens with unnamed number?
 app.use(express.static('build'))
-// app.use(cors())
 
-morgan.token('body', (req, res) => {
-	if (req.method === 'POST')
-		return JSON.stringify(req.body)
-	return null
+morgan.token('body', (req) => {
+  if (req.method === 'POST')
+    return JSON.stringify(req.body)
+  return null
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
 
 const PORT = process.env.PORT
-app.listen(PORT, (req, res, next) => {
-	console.log(`Server running on port ${PORT}`)
-}).on('error', console.log) //What should I do when port is in use and get EADDRINUSE??
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+}).on('error', console.log)
 
-app.get('/api/persons', (req, res) => {
-	Person.find({}).then(person => {
-		res.json(person)
-	})
-	.catch(error => next(error))
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(person => {
+    res.json(person)
+  })
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res, next) => {
-	const date = new Date().toString()
-	const personN = Person.estimatedDocumentCount()
-	personN
-		.then(response => {
-		res.send(`<div>Phonebook has info for ${response} people
-		<br></br>
-		${date}</div>`)})
-		.catch(error => next(error))
+  const date = new Date().toString()
+  Person.estimatedDocumentCount()
+    .then(response => {
+      res.send(`<div>Phonebook has info for ${response} people
+    <br></br>
+    ${date}</div>`)})
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-	const id = req.params.id
-	Person.findById(id)
-		.then(person => {
-			if (person)
-				res.json(person)
-			else
-				res.status(404).end()
-		})
-		.catch(error => next(error))
-	// if (person)
-	// 	res.json(person)
-	// else
-		// res.status(404).end()
+  const id = req.params.id
+  Person.findById(id)
+    .then(person => {
+      if (person)
+        res.json(person)
+      else
+        res.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-	// const body = req.body
-	const { name, number } = req.body
-	const id = req.params.id
-	// const person = {
-	// 	name: body.name,
-	// 	number: body.number
-	// }
-	Person.findByIdAndUpdate(
-		id,
-		{name, number},
-		{ new:true, runValidators:true, context: 'query', returnDocument: 'after'}
-	)
-	.then(updatedPerson => {
-		res.json(updatedPerson)
-	})
-	.catch(error => next(error))
+  const { name, number } = req.body
+  const id = req.params.id
+  Person.findByIdAndUpdate(
+    id,
+    { name, number },
+    { new:true, runValidators:true, context: 'query', returnDocument: 'after' })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-	const id = req.params.id
-	Person.findByIdAndRemove(id)
-		.then(response => {
-			res.status(204).end()
-		})
-		.catch(error => next(error))
+  const id = req.params.id
+  Person.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const validateBody = (body, res) => {
-	if (!body.name)
-		return res.status(404).json({ //RETUNR OR .end()???
-			error: 'name missing'
-		})
-	if (!body.number)
-		return res.status(404).json({
-			error: 'number missing'
-		})
-	return null
+app.post('/api/persons', (req, res, next) => {
+  const id = Math.floor(Math.random() * 1000000)
+  const body = req.body
+  const person = { ...body, id: id }
+  Person.create(person)
+    .then(response => {
+      res.json(response).end()
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.post('/api/persons', (req, res, next) => {
-	const id = Math.floor(Math.random() * 1000000)
-	const body = req.body
-	// const validate = validateBody(body, res)
-	// if (validate)
-	// 	return validate
-	const person = {...body, id: id}
-	Person.create(person)
-		.then(response => {
-			res.json(response).end()
-		})
-		.catch(error => next(error))
-	})
+app.use(unknownEndpoint)
 
-	const unknownEndpoint = (request, response) => {
-		response.status(404).send({ error: 'unknown endpoint' })
-	}
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
 
-	app.use(unknownEndpoint)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
-	const errorHandler = (error, request, response, next) => {
-		console.log(error.message)
+  next(error)
+}
 
-		if (error.name === 'CastError') {
-			return response.status(400).send({ error: 'malformatted id' })
-		} else if (error.name === 'ValidationError') {
-			return response.status(400).send({ error: error.message })
-		}
-		
-		next(error)
-	}
-
-	app.use(errorHandler)
+app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -1,71 +1,43 @@
 const mongoose = require('mongoose')
 
-// if (argvLen === 4 || argvLen > 5)
-// 	process.exit(1)
-
 const url = process.env.MONGODB_URI
-	// `mongodb+srv://fullstack:${password}@phonebook.jb7vwo2.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
-	.then(result => {
-		console.log("connected to mongoDB")
-	})
-	.catch((error) => {
-		console.log("error connecting to MongoDB", error.message)
-	})
+  .then(() => {
+    console.log('connected to mongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
-	type: String,
-	required: true,
-	minLength: 3
+    type: String,
+    required: true,
+    minLength: 3
   },
   number: {
-	type: String,
-	required: true,
-	minLength: 8,
-	validate: {
-		validator: (number) => {
-			const regex = /^(?:\d{2}-\d{5,}|\d{3}-\d{4,})$/
-			return regex.test(number)
-		},
-		message: props => `${props.value} is not a valid phone number`
-	}
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: {
+      validator: (number) => {
+        const regex = /^(?:\d{2}-\d{5,}|\d{3}-\d{4,})$/
+        return regex.test(number)
+      },
+      message: props => `${props.value} is not a valid phone number`
+    }
   }
 })
 
 //Change the default behaviour of toJSON on schema!
 personSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString()
-		delete returnedObject._id
-		delete returnedObject.__v
-	}
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
-
-// const Person = mongoose.model('Person', personSchema)
-
-// if (argvLen === 3) {
-// 	Person.find({}, 'name number').then(result => {
-// 		console.log('phonebook:')
-// 		result.forEach(person => {
-// 			console.log(`${person.name} ${person.number}`)
-// 		})
-// 		mongoose.connection.close()
-// 	})
-// }
-
-// if (argvLen === 5) {
-// 	const person = new Person({
-//   	name: process.argv[3],
-//   	number: Number(process.argv[4]),
-// 	})
-
-// 	person.save().then(result => {
-// 	console.log(`added ${result.name} number ${result.number} to phonebook`)
-//   	mongoose.connection.close()
-// 	})
-// }
 
 module.exports = mongoose.model('Person', personSchema)
